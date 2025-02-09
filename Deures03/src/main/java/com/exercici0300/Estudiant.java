733363cf3f28b1b919be6c1c22693967fb67d3cb
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2025-02-09 01:07:07 +0100

    feat: pujada de fitxers, i canvis de codi (pendents de comprobar)

---
+++ Deures03/src/main/java/com/exercici0300/Estudiant.java
@@ -0,0 +1,47 @@
+package com.exercici0300;
+
+public class Estudiant {
+    private String nom;
+    private String id;
+    private static int comptadorEstudiants;
+    private static final int CAPACITAT_MAXIMA = 5;
+
+    // Constructor
+    public Estudiant(String nom, String id) {
+        if (comptadorEstudiants < CAPACITAT_MAXIMA){
+            this.nom = nom;
+            this.id = id;
+            comptadorEstudiants++;
+        }
+        else { 
+            System.out.println("Error: No es poden registrar més estudiants. Capacitat màxima assolida.");
+        }
+        
+    }
+
+    //Getters i setters
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    // Mètodes públics estàtics
+    public static int getComptadorEstudiants() {
+        return -1;
+    }
+
+    public static boolean hiHaCapacitat() {
+        return false;
+    }
+}
---
+++ Deures03/src/main/java/com/exercici0301/ControlTemperatura.java
@@ -0,0 +1,30 @@
+package com.exercici0301;
+
+public class ControlTemperatura {
+    private static double temperaturaTotal;
+    private static int comptadorZones;
+    private String nomZona;
+    private double temperatura;
+
+    public ControlTemperatura(String nomZona, double temperatura) {
+        this.nomZona = nomZona;
+        this.temperatura = temperatura;
+        this.temperaturaTotal = temperatura +temperaturaTotal;
+        comptadorZones++;
+    }
+
+    public double getTemperatura() {
+        return 0.0;
+    }
+
+    public void ajustaTemperatura(double novaTemperatura) {
+        temperaturaTotal -= temperatura;
+        novaTemperatura = temperatura;
+        temperaturaTotal += novaTemperatura;
+    }
+
+    public static double getTemperaturaMitjana() {
+        return temperaturaTotal/comptadorZones;
+    }
+}
+
